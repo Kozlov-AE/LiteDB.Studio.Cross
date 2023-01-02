7317cf89cbcf18850b9760522bab008c41731e34
A_Kozlov <akozlov@b-rain.org>
2023-01-02 20:33:53 +0300

    First layout

---
+++ LiteDB.Studio.Cross/Views/LeftSideView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LiteDB.Studio.Cross.Views;
+
+public partial class LeftSideView : UserControl
+{
+    public LeftSideView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ LiteDB.Studio.Cross/Views/RigthSideView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LiteDB.Studio.Cross.Views;
+
+public partial class RigthSideView : UserControl
+{
+    public RigthSideView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
