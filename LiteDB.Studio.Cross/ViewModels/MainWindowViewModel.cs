6a818bf2fca79cd1a04f527940aa6f1a5d089701
Kozlov_AE <Allek-007@list.ru>
2023-01-03 21:24:15 +0300

    Create connection sting logic

--- LiteDB.Studio.Cross/ViewModels/MainWindowViewModel.cs
+++ LiteDB.Studio.Cross/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,63 @@
-﻿namespace LiteDB.Studio.Cross.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LiteDB.Studio.Cross.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private ConnectionString _connectionString;
+        //private const long MB = 1024 * 1024;
+        private long MB = 1024 * 1024;
+        
+        [ObservableProperty]
+        private bool _isLoadDatabaseNeeded = true;
+        [ObservableProperty]
+        private DbConnectionOptionsViewModel _connectionOpts;
+
+        public MainWindowViewModel()
+        {
+            _connectionString = new ConnectionString();
+            SetConnectionVM(_connectionString);
+        }
+
+        private DbConnectionOptionsViewModel SetConnectionVM(ConnectionString cs)
+        {
+            var result = new DbConnectionOptionsViewModel()
+            {
+                IsDirect = cs.Connection == ConnectionType.Direct,
+                IsShared = cs.Connection == ConnectionType.Shared,
+                DbPath = cs.Filename,
+                Password = cs.Password,
+                InitSize = (cs.InitialSize/MB).ToString(),
+                IsReadOnly = cs.ReadOnly,
+                IsUpgrade = cs.Upgrade
+            };
+            if (cs.Collation != null)
+            {
+                result.Culture = _connectionString.Collation.Culture.ToString();
+                result.Sort = cs.Collation.SortOptions.ToString();
+            }
+            return result;
+        }
+
+        private void ConfigureConnectionString(DbConnectionOptionsViewModel vm)
+        {
+            _connectionString.Connection = vm.IsDirect ? ConnectionType.Direct : ConnectionType.Shared;
+            _connectionString.Filename = vm.DbPath;
+            _connectionString.ReadOnly = vm.IsReadOnly;
+            _connectionString.Upgrade = vm.IsUpgrade;
+            _connectionString.Password = vm.Password?.Trim().Length > 0 ? vm.Password?.Trim() : null;
+            if (int.TryParse(vm.InitSize,out var initSize))
+            {
+                _connectionString.InitialSize = initSize * MB;
+            }
+
+            if (!string.IsNullOrEmpty(vm.Culture))
+            {
+                var collation = vm.Culture;
+                if (!string.IsNullOrEmpty(vm.Sort))
+                    collation += "/" + vm.Sort;
+                _connectionString.Collation = new Collation(collation);
+            }
+        }
     }
 }
--- LiteDB.Studio.Cross/Views/ConnectionView.axaml.cs
+++ LiteDB.Studio.Cross/Views/ConnectionView.axaml.cs
@@ -1,14 +1,51 @@
-using Avalonia;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace LiteDB.Studio.Cross.Views;
 
 public partial class ConnectionView : UserControl
 {
+    private Button _openFileButton;
+    private TextBox _selectedFile;
+    private ComboBox _cultureBox;
+    private ComboBox _sortBox;
+    
+    Window GetWindow() => this.VisualRoot as Window ?? throw new NullReferenceException("Invalid Owner");
     public ConnectionView()
     {
         InitializeComponent();
+        _openFileButton = this.Find<Button>("OpenFileButton");
+        if (_openFileButton != null) _openFileButton.Click += OpenFileButtonOnClick;
+        _cultureBox = this.Find<ComboBox>("CultureBox");
+        if (_cultureBox != null) _cultureBox.Items = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            .Select(x => x.LCID)
+            .Distinct()
+            .Where(x => x != 4096)
+            .Select(x => CultureInfo.GetCultureInfo(x).Name)
+            .ToList();
+        _sortBox = this.Find<ComboBox>("SortBox");
+        if (_sortBox != null) _sortBox.Items = Enum.GetNames(typeof(CompareOptions));
+    }
+
+    private async void OpenFileButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        var uri = Assembly.GetEntryAssembly()?.GetModules().FirstOrDefault()?.FullyQualifiedName;
+        var initialFileName = uri == null ? null : System.IO.Path.GetFileName(uri);
+        var initialDirectory = uri == null ? null : System.IO.Path.GetDirectoryName(uri);
+
+        var result = await new OpenFileDialog()
+        {
+            Title = "Open file",
+            Directory = initialDirectory,
+            InitialFileName = initialFileName
+        }.ShowAsync(GetWindow());
+        _selectedFile = this.Find<TextBox>("SelectedFile");
+        if (_selectedFile != null) _selectedFile.Text = result?.First();
     }
 
     private void InitializeComponent()
