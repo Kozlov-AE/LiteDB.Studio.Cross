3d31363272acf2f8f09fe088efe19d0d4418bcac
Kozlov_AE <Allek-007@list.ru>
2023-01-22 18:45:51 +0300

    Request service started

---
+++ LiteDB.Studio.Cross/Models/DbQuerryResultModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace LiteDB.Studio.Cross.Models {
+    public class DbQuerryResultModel {
+        public DbCollectionModel Collection { get; set; } = new DbCollectionModel();
+        public List<CollectionItem> Items { get; set; }
+    }
+
+    public class DbCollectionModel {
+        public string CollectionName { get; set; }
+        public HashSet<PropertyModel> Properties { get; set; } = new HashSet<PropertyModel>();
+    }
+
+    public class CollectionItem {
+        public Dictionary<string, dynamic> Values { get; set; } = new Dictionary<string, dynamic>();
+    }
+}
