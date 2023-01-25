89eee1351a6041e103c57a706c1bf99ba2ce7e05
Kozlov-AE <Allek-007@list.ru>
2023-01-26 00:42:24 +0300

    Added History service

---
+++ LiteDB.Studio.Cross/Interfaces/ILastOpenedDbStoryService.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace LiteDB.Studio.Cross.Interfaces {
+    public interface ILastOpenedDbStoryService {
+        void AddToStory(string path);
+        IEnumerable<string> GetStory();
+    }
+}
