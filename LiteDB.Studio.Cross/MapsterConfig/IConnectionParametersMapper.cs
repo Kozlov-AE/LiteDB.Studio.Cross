28dc271456e802ac2bc6eee2bdafcd9996247ddd
Kozlov-AE <Allek-007@list.ru>
2023-02-13 06:17:44 +0300

    Removed Mapster

--- LiteDB.Studio.Cross/MapsterConfig/IConnectionParametersMapper.cs
+++ LiteDB.Studio.Cross/MapsterConfig/IConnectionParametersMapper.cs
@@ -1,11 +1,11 @@
-using LiteDB.Studio.Cross.Contracts.DTO;
-using LiteDB.Studio.Cross.ViewModels;
-using Mapster;
-
-namespace LiteDB.Studio.Cross.MapsterConfig {
-    [Mapper]
-    public interface IConnectionParametersMapper {
-        ConnectionParametersDto MapTo(ConnectionParametersViewModel vm);
-        ConnectionParametersViewModel MapTo(ConnectionParametersDto dto);
-    }
-}
+// using LiteDB.Studio.Cross.Contracts.DTO;
+// using LiteDB.Studio.Cross.ViewModels;
+// using Mapster;
+//
+// namespace LiteDB.Studio.Cross.MapsterConfig {
+//     [Mapper]
+//     public interface IConnectionParametersMapper {
+//         ConnectionParametersDto MapTo(ConnectionParametersViewModel vm);
+//         ConnectionParametersViewModel MapTo(ConnectionParametersDto dto);
+//     }
+// }
--- LiteDB.Studio.Cross/MapsterConfig/RegisterMapper.cs
+++ LiteDB.Studio.Cross/MapsterConfig/RegisterMapper.cs
@@ -1,14 +1,14 @@
-using LiteDB.Studio.Cross.Contracts.DTO;
-using LiteDB.Studio.Cross.ViewModels;
-using Mapster;
-
-namespace LiteDB.Studio.Cross.MapsterConfig {
-    public class RegisterMapper : IRegister{
-        public void Register(TypeAdapterConfig config) {
-            config.NewConfig<ConnectionParametersDto, ConnectionParametersViewModel>()
-                .RequireDestinationMemberSource(true);
-            config.NewConfig<DataBaseDto, DatabaseViewModel>()
-                .RequireDestinationMemberSource(true);
-        }
-    }
-}
+// using LiteDB.Studio.Cross.Contracts.DTO;
+// using LiteDB.Studio.Cross.ViewModels;
+// using Mapster;
+//
+// namespace LiteDB.Studio.Cross.MapsterConfig {
+//     public class RegisterMapper : IRegister{
+//         public void Register(TypeAdapterConfig config) {
+//             config.NewConfig<ConnectionParametersDto, ConnectionParametersViewModel>()
+//                 .RequireDestinationMemberSource(true);
+//             config.NewConfig<DataBaseDto, DatabaseViewModel>()
+//                 .RequireDestinationMemberSource(true);
+//         }
+//     }
+// }
