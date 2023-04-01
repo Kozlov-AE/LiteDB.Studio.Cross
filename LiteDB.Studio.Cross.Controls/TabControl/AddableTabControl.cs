a4cfea3caabd762321a7d03917d09bbbd6eb7a91
Kozlov-AE <allek-007@list.ru>
2023-04-01 20:09:32 +0300

    Made addable tab control

--- LiteDB.Studio.Cross.Controls/TabControl/AddableTabControl.cs
+++ LiteDB.Studio.Cross.Controls/TabControl/AddableTabControl.cs
@@ -1,5 +1,27 @@
-﻿namespace LiteDB.Studio.Cross.Controls.TabControl {
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+
+namespace LiteDB.Studio.Cross.Controls.TabControl {
     public class AddableTabControl : Avalonia.Controls.TabControl{
-        
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+            base.OnApplyTemplate(e);
+            var addButton = this.FindControl<Button>("PART_AddButton");
+            if (addButton != null) {
+                addButton.Click += (sender, args) => {
+                    var e_ = new RoutedEventArgs(ClickOnAddButtonEvent);
+                    RaiseEvent(e_);
+                    e_.Handled = true;
+                };
+            }
+        }
+
+        public static readonly RoutedEvent<RoutedEventArgs> ClickOnAddButtonEvent 
+            = RoutedEvent.Register<AddableTabControl, RoutedEventArgs>(nameof(ClickOnAddButtonEvent), RoutingStrategies.Bubble);
+        public event EventHandler<RoutedEventArgs> ClickOnAddButton {
+            add => AddHandler(ClickOnAddButtonEvent, value);
+            remove => RemoveHandler(ClickOnAddButtonEvent, value);
+        }
     }
 }
